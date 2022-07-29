4ce1378f4ce8a5dd0fffe72ab94434eb1baee177
Igor Stepanov <igorstepanov@hotmail.com>
2022-07-29 13:23:07 +0300

    build(Todo List): подготовить проект

---
+++ app.js
@@ -0,0 +1,34 @@
+const tasks = [
+  {
+    _id: '5d2ca9e2e03d40b326596aa7',
+    completed: true,
+    body:
+      'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
+    title: 'Eu ea incididunt sunt consectetur fugiat non.',
+  },
+  {
+    _id: '5d2ca9e29c8a94095c1288e0',
+    completed: false,
+    body:
+      'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+    title:
+      'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+  },
+  {
+    _id: '5d2ca9e2e03d40b3232496aa7',
+    completed: true,
+    body:
+      'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
+    title: 'Eu ea incididunt sunt consectetur fugiat non.',
+  },
+  {
+    _id: '5d2ca9e29c8a94095564788e0',
+    completed: false,
+    body:
+      'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+    title:
+      'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+  },
+];
+
+(function(arrOfTasks) {})(tasks);
