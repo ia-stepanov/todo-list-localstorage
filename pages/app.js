c472f836c8a8c4ffbaf94b96cdee036c66ddb3a6
Igor Stepanov <igorstepanov@hotmail.com>
2022-08-01 15:32:03 +0300

    build(Todo List): создать функцию для добавления новой задачи в список задач

--- pages/app.js
+++ pages/app.js
@@ -35,8 +35,13 @@ const tasks = [
 
   // Elements UI
   const listContainer = document.querySelector('.tasks-list-section .list-group');
+  const form = document.forms['addTask'];
+  const inputTitle = form.elements['title'];
+  const inputBody = form.elements['body'];
 
+  // Events
   renderAllTasks(objOfTasks);
+  form.addEventListener('submit', onFormSubmitHandler);
 
   function renderAllTasks(tasksList) {
     if (!tasksList) {
@@ -80,5 +85,35 @@ const tasks = [
     li.appendChild(article);
 
     return li;
+  }
+
+  function onFormSubmitHandler(e) {
+    e.preventDefault();
+
+    const titleValue = inputTitle.value;
+    const bodyValue = inputBody.value;
+
+    if (!titleValue || !bodyValue) {
+      alert('Пожалуйста, введите заголовок и/или текст.');
+      return;
+    }
+
+    const task = createNewTask(titleValue, bodyValue);
+    const listItem = listItemTemplate(task);
+    listContainer.insertAdjacentElement('afterbegin', listItem);
+    form.reset()
+  }
+
+  function createNewTask(title, body) {
+    const newTask = {
+      title,
+      body,
+      completed: false,
+      _id: `task-${Math.random()}`,
+    };
+
+    objOfTasks[newTask._id] = newTask;
+
+    return { ...newTask };
   }
 })(tasks);
